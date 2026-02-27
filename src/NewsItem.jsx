5e48a0ce209e29ccf0375b72f20ff07e27b00160
kuro <kkrmlove1011@gmail.com>
2026-02-27 15:23:34 +0900

    🎨 【課題1】の対応

---
+++ src/NewsItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NewsItem = ({ post }) => {
+  return (
+    <li className="p-6 mb-8 border">
+      <div className="flex justify-between">
+        <time>{post.createdAt}</time>
+        <div className="flex gap-2">
+          {post.categories?.map((category) => {
+            return <span>{category}</span>;
+          })}
+        </div>
+      </div>
+      <h1 className="text-left text-4xl">{post.title}</h1>
+      <p className="pt-2 text-left">{post.content}</p>
+    </li>
+  );
+};
+
+export default NewsItem;
---
+++ src/NewsList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NewsItem from "./NewsItem";
+import { posts } from "./data/posts";
+
+const NewsList = () => {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <NewsItem post={post} key={post.id} />
+      ))}
+    </ul>
+  );
+};
+
+export default NewsList;
