430fc367794b18574e741d7263e4cc4d44cc4f97
kuro <kkrmlove1011@gmail.com>
2026-03-02 19:10:23 +0900

    🎨 Linkの設置と日付をJP式で表示

--- src/NewsItem.jsx
+++ src/NewsItem.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { Link } from "react-router";
 
 const NewsItem = ({ post }) => {
   return (
     <li className="p-6 mb-8 border">
-      <div className="flex justify-between">
-        <time>{post.createdAt}</time>
-        <div className="flex gap-2">
-          {post.categories?.map((category) => {
-            return <span>{category}</span>;
-          })}
+      <Link to={`/post/${post.id}`}>
+        <div className="flex justify-between">
+          <time> {new Date(post.createdAt).toLocaleDateString("ja-JP")}</time>
+          <div className="flex gap-2">
+            {post.categories?.map((category) => {
+              return <span>{category}</span>;
+            })}
+          </div>
         </div>
-      </div>
-      <h1 className="text-left text-4xl">{post.title}</h1>
-      <p
-        className="pt-2 text-left"
-        dangerouslySetInnerHTML={{ __html: post.content }}
-      />
+        <h1 className="text-left text-4xl">{post.title}</h1>
+        <p
+          className="pt-2 text-left"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </Link>
     </li>
   );
 };
