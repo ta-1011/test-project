f07a86bc4ec09c8d5ed417968cd2174293ad502e
kuro <kkrmlove1011@gmail.com>
2026-03-03 20:46:24 +0900

    🎨 APIで記事データを取得し表示

--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { posts } from "../data/posts";
+
+const API_BASE_URL =
+  "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev";
 
 const NewsDetail = () => {
   const { id } = useParams();
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const post = posts.find((p) => {
-    return p.id === Number(id); //data内のidは、文字列でなくナンバーなので、ただの「id」だけでは×
-  });
+  const fetchPost = async () => {
+    try {
+      const res = await fetch(`${API_BASE_URL}/posts/${id}`);
+      const { post } = await res.json(); //詳細ページは
+      setPost(post);
+    } catch (error) {
+      setError("記事の取得に失敗しました。");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  if (loading) {
+    return <p>ローディング中です</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   if (!post) {
     return (
