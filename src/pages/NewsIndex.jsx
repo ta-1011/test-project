caa6ef619564eab57e5e9923c158439957b6877d
kuro <kkrmlove1011@gmail.com>
2026-03-04 19:25:41 +0900

    🎨 APIで記事データを取得（一覧ページ）

--- src/pages/NewsIndex.jsx
+++ src/pages/NewsIndex.jsx
@@ -1,11 +1,55 @@
 import React from "react";
 import NewsItem from "./NewsItem";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { API_BASE_URL } from "../constants";
 
 const NewsIndex = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchPost = async () => {
+    try {
+      const res = await fetch(`${API_BASE_URL}/posts`);
+      const { posts } = await res.json();
+      setPosts(posts);
+    } catch (error) {
+      setError("記事の取得に失敗しました。");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  if (loading) {
+    return <p>記事を読み込み中です。</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!posts) {
+    return (
+      <>
+        <p className="mt-8">記事が見つかりませんでした。</p>
+        <div>
+          <Link to="/" className="topButton">
+            トップへ戻る
+          </Link>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="mt-8">
-      <ul>
+      <h1 className="text-left text-4xl">記事一覧</h1>
+      <ul className="mt-4">
         {posts.map((post) => (
           <NewsItem post={post} key={post.id} />
         ))}
