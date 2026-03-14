1ad1c422e17a0a26cde943514d4142589a00fe79
kuro <kkrmlove1011@gmail.com>
2026-03-14 18:07:04 +0900

    🎨 react-router-domからimport

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BaseLayout from "./layouts/BaseLayout";
 import NewsIndex from "./pages/NewsIndex";
 import NewsDetail from "./pages/NewsDetail";
--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 const BaseLayout = () => {
   return (
